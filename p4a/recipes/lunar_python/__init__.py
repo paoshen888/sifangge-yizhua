2ba7d93914a2cceea95db250f7883872ab856352
paoshen888 <1187044@qq.com>
2026-08-03 18:59:13 +0800

    fix: use exact files.pythonhosted.org URL with hash

--- p4a/recipes/lunar_python/__init__.py
+++ p4a/recipes/lunar_python/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class LunarPythonRecipe(PythonRecipe):
     version = '1.4.8'
-    url = 'https://files.pythonhosted.org/packages/source/l/lunar-python/lunar-python-{version}.tar.gz'
+    url = 'https://files.pythonhosted.org/packages/59/45/5154c95ae7feaab7ca508e71c3288692c09952dfe33b03b7c2f18a32e2cd/lunar_python-{version}.tar.gz'
     depends = ['setuptools']
     call_hostpython_via_targetpython = False
 
