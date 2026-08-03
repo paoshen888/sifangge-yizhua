b2d79589af4e9988542e099724c7ce0ae01f8ad3
paoshen888 <1187044@qq.com>
2026-08-03 18:42:16 +0800

    add lunar_python p4a recipe

---
+++ p4a/recipes/lunar_python/__init__.py
@@ -0,0 +1,9 @@
+from pythonforandroid.recipe import PythonRecipe
+
+class LunarPythonRecipe(PythonRecipe):
+    version = '1.4.8'
+    url = 'https://pypi.python.org/packages/source/l/lunar-python/lunar-python-{version}.tar.gz'
+    depends = ['setuptools']
+    call_hostpython_via_targetpython = False
+
+recipe = LunarPythonRecipe()
