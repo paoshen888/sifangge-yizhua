d9da1169ea544e74cae37651f370644de67c3496
paoshen888 <1187044@qq.com>
2026-08-03 18:48:06 +0800

    fix: use files.pythonhosted.org instead of pypi.python.org

--- p4a/recipes/lunar_python/__init__.py
+++ p4a/recipes/lunar_python/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class LunarPythonRecipe(PythonRecipe):
     version = '1.4.8'
-    url = 'https://pypi.python.org/packages/source/l/lunar-python/lunar-python-{version}.tar.gz'
+    url = 'https://files.pythonhosted.org/packages/source/l/lunar-python/lunar-python-{version}.tar.gz'
     depends = ['setuptools']
     call_hostpython_via_targetpython = False
 
